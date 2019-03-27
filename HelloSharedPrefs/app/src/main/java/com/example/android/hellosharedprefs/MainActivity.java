04a57283c0acc2a2e7727fabdeeab8360b35a6b8
hooploop <mingshen0516@gmail.com>
2019-03-27 10:25:22 +0800

    Add files via upload

---
+++ HelloSharedPrefs/app/src/main/java/com/example/android/hellosharedprefs/MainActivity.java
@@ -0,0 +1,170 @@
+/*
+ * Copyright (C) 2016 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.android.hellosharedprefs;
+
+import android.content.SharedPreferences;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+/**
+ * HelloSharedPrefs is an adaptation of the HelloToast app from chapter 1.
+ * It includes:
+ * - Buttons for changing the background color.
+ * - Maintenance of instance state.
+ * - Themes and styles.
+ * - Read and write shared preferences for the current count and the color.
+ * <p>
+ * This is the starter code for HelloSharedPrefs.
+ */
+
+
+public class MainActivity extends AppCompatActivity {
+    // Current count
+    private int mCount = 0;
+    // Current background color
+    private int mColor;
+    // Text view to display both count and color
+    private TextView mShowCountTextView;
+
+    // Key for current count
+    private final String COUNT_KEY = "count";
+    // Key for current color
+    private final String COLOR_KEY = "color";
+
+//initialize preferences
+    private SharedPreferences mPreferences;
+    private String sharedPrefFile = "android.hellosharedprefs";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Initialize views, color, preferences
+        mShowCountTextView = findViewById(R.id.count_textview);
+        mColor = ContextCompat.getColor(this,
+                R.color.default_background);
+
+        //initialize shared preferences
+        mPreferences = getSharedPreferences(sharedPrefFile, MODE_PRIVATE);
+
+        //RESTORE RPEFERENCES FROM LAST SAVED:
+        //get count from preferences with COUNTKEY & assign to mCount variable
+        mCount = mPreferences.getInt(COUNT_KEY, 0);
+
+        //update value of main TextView with new count
+        mShowCountTextView.setText(String.format("%s",mCount));
+
+        //get colour form preferences with COLOR_KEY & assign to mColor variable
+        mColor = mPreferences.getInt(COLOR_KEY, mColor);
+
+        //update bg color of main text
+        mShowCountTextView.setBackgroundColor(mColor);
+
+
+        // Restore the saved instance state.
+   /*     if (savedInstanceState != null) {
+
+            mCount = savedInstanceState.getInt(COUNT_KEY);
+            if (mCount != 0) {
+                mShowCountTextView.setText(String.format("%s", mCount));
+            }
+
+            mColor = savedInstanceState.getInt(COLOR_KEY);
+            mShowCountTextView.setBackgroundColor(mColor);
+        } */
+    }
+
+    @Override //save preferences in onPause()
+    protected  void onPause(){
+        super.onPause();
+        //shared preferences editor is required to write to the shared preferences object
+        SharedPreferences.Editor preferencesEditor = mPreferences.edit();
+        preferencesEditor.putInt(COUNT_KEY, mCount);
+        preferencesEditor.putInt(COLOR_KEY,mColor);
+        //call apply to save preferences
+        preferencesEditor.apply();
+    }
+
+    /**
+     * Handles the onClick for the background color buttons. Gets background
+     * color of the button that was clicked, and sets the TextView background
+     * to that color.
+     *
+     * @param view The view (Button) that was clicked.
+     */
+    public void changeBackground(View view) {
+        int color = ((ColorDrawable) view.getBackground()).getColor();
+        mShowCountTextView.setBackgroundColor(color);
+        mColor = color;
+    }
+
+    /**
+     * Handles the onClick for the Count button. Increments the value of the
+     * mCount global and updates the TextView.
+     *
+     * @param view The view (Button) that was clicked.
+     */
+    public void countUp(View view) {
+        mCount++;
+        mShowCountTextView.setText(String.format("%s", mCount));
+    }
+
+    /**
+     * Saves the instance state if the activity is restarted (for example,
+     * on device rotation.) Here you save the values for the count and the
+     * background color.
+     *
+     * @param outState The state data.
+     */
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putInt(COUNT_KEY, mCount);
+        outState.putInt(COLOR_KEY, mColor);
+    }
+
+    /**
+     * Handles the onClick for the Reset button. Resets the global count and
+     * background variables to the defaults and resets the views to those
+     * default values.
+     *
+     * @param view The view (Button) that was clicked.
+     */
+    public void reset(View view) {
+        // Reset count
+        mCount = 0;
+        mShowCountTextView.setText(String.format("%s", mCount));
+
+        // Reset color
+        mColor = ContextCompat.getColor(this,
+                R.color.default_background);
+        mShowCountTextView.setBackgroundColor(mColor);
+
+        //CLEAR PREFERENCES
+        // after the color and count are reset, get an editor for SharedPreferences object:
+        SharedPreferences.Editor preferencesEditor = mPreferences.edit();
+        //delete all shared preferences
+        preferencesEditor.clear();
+        //apply changes
+        preferencesEditor.apply();
+    }
+}
